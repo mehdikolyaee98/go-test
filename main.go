4f9cf2da8878afb393f4bad5f27602c9847e6474
mehdi <mehdi.kolyaee@gmail.com>
2023-09-20 16:05:09 +0330

    initial commit

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"test/test"
+)
+
+func main() {
+	test.Test()
+	fmt.Println("nhdflgbikusheipguh")
+	var mehdiina string = "عذقهعثقبعغثقعغثعغثقبثغذقبثقغعثققاثع9ثبعاب.09.00"
+	fmt.Println(mehdiina)
+
+	var name string = "company netis"
+	var bime string = "mihan"
+	fmt.Println("welcome to ", bime, " zir majmoee", ",", name)
+	fmt.Println(name)
+	fmt.Println(bime)
+	fmt.Println("Mehdi the Best")
+	mamad()
+
+	var name1 string = "mamad"
+	var i int = 78
+	fmt.Printf("hey %s kholase ke %d gerdo darim", name1, i)
+	var m, c, h string = "bviuhsei", "rtiogjhrsoijtg", "jbngflkuehsr"
+	fmt.Println(m)
+	fmt.Println(c)
+	fmt.Println(h)
+	var (
+		s string = "دختهبتهخ"
+		q int    = 354
+	)
+	fmt.Println(s)
+	fmt.Println(q)
+	namemamad := "bhvj"
+	namemamad = "ثالهثع"
+	fmt.Print(namemamad)
+}
+
+func mamad() {
+
+	fmt.Println("Mehdi the Best")
+}
---
+++ test/test.go
@@ -0,0 +1,7 @@
+package test
+
+import "fmt"
+
+func Test() {
+	fmt.Print("mamad nabodi bbeebini")
+}
