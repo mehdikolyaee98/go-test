c139b3d685a8e9673e9ebc12b599ec785e7470ad
mehdi <mehdi.kolyaee@gmail.com>
2023-09-27 18:16:31 +0330

    working until ananymous functions

--- main.go
+++ main.go
@@ -1,43 +1,298 @@
 package main
 
-import (
-	"fmt"
-	"test/test"
-)
+import "crypto/x509"
+
+const pi float64 = 3.14
 
 func main() {
-	test.Test()
-	fmt.Println("nhdflgbikusheipguh")
-	var mehdiina string = "عذقهعثقبعغثقعغثعغثقغذقبثقغعثققاثع9ثبعاب.09.00"
-	fmt.Println(mehdiina)
+	//test.Test()
+	//fmt.Println("nhdflgbikusheipguh")
+	//var mehdiina string = "عذقهعثقبعغثقعغثعغثقغذقبثقغعثققاثع9ثبعاب.09.00"
+	//fmt.Println(mehdiina)
+	//
+	//var name string = "company netis"
+	//var bime string = "mihan"
+	//fmt.Println("welcome to ", bime, " zir majmoee", ",", name)
+	//fmt.Println(name)
+	//fmt.Println(bime)
+	//fmt.Println("Mehdi the Best")
+	//mamad()
+	//
+	//var name1 string = "mamad"
+	//var i int = 78
+	//fmt.Printf("hey %s kholase ke %d gerdo darim", name1, i)
+	//var m, c, h string = "bviuhsei", "rtiogjhrsoijtg", "jbngflkuehsr"
+	//fmt.Println(m)
+	//fmt.Println(c)
+	//fmt.Println(h)
+	//var (
+	//	s string = "دختهبتهخ"
+	//	q int    = 354
+	//)
+	//fmt.Println(s)
+	//fmt.Println(q)
 
-	var name string = "company netis"
-	var bime string = "mihan"
-	fmt.Println("welcome to ", bime, " zir majmoee", ",", name)
-	fmt.Println(name)
-	fmt.Println(bime)
-	fmt.Println("Mehdi the Best")
-	mamad()
+	//جالسهههههههههههه دوووووووووممممممممممممم
+	//namemamad := "bhvj"
+	//namemamad = "ثالهثع"
+	//fmt.Print(namemamad)
+	////
+	//contury := "mamalabad"
+	//fmt.Println(contury)
+	//var khali int
+	//fmt.Println(khali)
+	//var kli float64
+	//fmt.Printf("%.2f", kli)
+	//var name string
+	//fmt.Print("esmet ro vared kon: ")
+	//fmt.Scanf("%s", &name)
+	//fmt.Println("salam,", name)
+	//
+	//var name string
+	//var ismamad bool
+	//fmt.Print("esmet ro bedeh aya ismamadi to: ")
+	//fmt.Scanf("%s %t ", &name, &ismamad)
+	//fmt.Println(name, ismamad)
 
-	var name1 string = "mamad"
-	var i int = 78
-	fmt.Printf("hey %s kholase ke %d gerdo darim", name1, i)
-	var m, c, h string = "bviuhsei", "rtiogjhrsoijtg", "jbngflkuehsr"
-	fmt.Println(m)
-	fmt.Println(c)
-	fmt.Println(h)
-	var (
-		s string = "دختهبتهخ"
-		q int    = 354
-	)
-	fmt.Println(s)
-	fmt.Println(q)
-	namemamad := "bhvj"
-	namemamad = "ثالهثع"
-	fmt.Print(namemamad)
+	//جلسه سومممممممممم
+
+	//var a string
+	//var b int
+	//fmt.Print("dosmamad hastam ba ye adad:")
+	//count, err := fmt.Scanf("%s %d ", &a, &b)
+	//fmt.Println("count:", count)
+	//fmt.Println("error:", err)
+	//fmt.Println("a:", a)
+	//fmt.Println("b:", b)
+	//var mamalo int = 42
+	//fmt.Printf("yegfljuwyaegfluwaye=%v taypfolan %T /n", mamalo, mamalo)
+	//fmt.Printf("taype of %v 	\n", reflect.TypeOf(1568658)) //توی پرانتز ریفلکس میشه نام متغیر رو اورد
+	//var f float64 = 45.56
+	//var i int = int(f)
+	//fmt.Printf("%v\n", i)
+	//var i int = 42
+	//var s string = strconv.Itoa(i)
+	//fmt.Printf("%q", s)
+	//var s string = "200"
+	//i, err := strconv.Atoi(s)
+	//fmt.Printf("%v,%T\n", i, i)
+	//fmt.Printf("%v,%T", err, err)
+
+	//var s string = "200abc"
+	//i, err := strconv.Atoi(s)
+	//fmt.Printf("%v,%T\n", i, i)
+	//fmt.Printf("%v,%T", err, err)
+	//const hname, hage = "rjghyfveiuwor", 15
+	//fmt.Printf("%v %d", hname, hage)
+	//000000000000000000000000000000000000000000000
+	//var radius float64 = 5.0
+	//var area float64
+	//area = pi * radius * radius
+	//fmt.Println("dayereh mamad ma :", area)
+	//0000000000000000000000000000000000000
+	//var i int = 1
+	//i--
+	//fmt.Println(i)
+	//جلسه چهارم
+	//var mmal string = "amat"
+	//if mmal == "amat" {
+	//	fmt.Println("amat adam khobieh")
+	//} else {
+	//	fmt.Println("amat kharabe")
+	//}
+	//000000000000000000000000000000000
+	//car := "praid"
+	//if car == "praid" {
+	//	fmt.Println("praid ro ridan nasakhtan")
+	//} else if car == "pezho" {
+	//	fmt.Println("irankhodrohamrideh")
+	//} else if car == "camery" {
+	//	fmt.Println("khobeh bad nist")
+	//} else {
+	//	fmt.Println("irani ro bikhial sho namosan")
+	//}
+	//0000000000000000000000000000000000000000000
+	//var i = 10
+	//switch i {
+	//case 10:
+	//	fmt.Println(" i is 10")
+	//case 100, 200, 110:
+	//	fmt.Println("i is 100 ya 200 ya 110")
+	//default:
+	//	fmt.Println("ya100 ya200 ya110 ya10 nist")
+	//}
+	//0000000000000000000000000000000000000000000000000000000
+	//var i int = 10
+	//switch i {
+	//case -5:
+	//	fmt.Println("-5")
+	//	fallthrough
+	//case 10:
+	//	fmt.Println("10")
+	//	//fallthrough
+	//case 100:
+	//	fmt.Println("100")
+	//	fallthrough
+	//default:
+	//	fmt.Println("ijuhrego0uise")
+	//
+	//}
+	//00000000000000000000000000000000
+	//var a, b int = 10, 20
+	//switch {
+	//case a+b == 30:
+	//	fmt.Println("mosavi 30 in dota")
+	//case a+b => 30:
+	//	fmt.Println("lejrg;iuehgiuehgiehugriueh")
+	//default:
+	//	fmt.Println("gr656465486546846846874684")
+	//}
+	////000000000000000000000000000000000000000000000
+	//for i := 1; i <= 16; i++ {
+	//	fmt.Println(i * i)
+	//}
+
+	//i := 1
+	//for i <= 5 {
+	//	fmt.Println(i * i)
+	//	i += 1
+	//}
+
+	//	sum := 0
+	//	for {
+	//		sum += 1
+	//	}
+	//	fmt.Println(1)
+	//}
+
+	//func mamad() {
+	//	fmt.Println("Mehdi the Best")
+	//
+	//}
+
+	//for i := 1; i <= 5; i++ {
+	//	if i == 3 {
+	//		break
+	//	}
+	//	fmt.Println(i)
+	//}
+	//for i := 1; i <= 6; i++ {
+	//	if i == 3 {
+	//		continue
+	//	}
+	//	fmt.Println(i)
+	//}
+	//00000000000000000000000
+	//var araeadabi [3]int = [3]int{10, 10, 10}
+	//araeadabi := [3]int{10, 10, 10}
+	//araeadabi := [...]int{10, 10, 10}
+
+	//var miveh [3]string = [3]string{"sib", "porteqal", "anar"}
+	//fmt.Println(miveh)
+	//
+	//adad := [...]int{2, 3, 3}
+	//fmt.Println(len(adad))
+	//
+	//adadd := [3]int{2, 3, 3}
+	//fmt.Println(len(adadd))
+	//
+	//var miveha [3]string = [3]string{"sib", "porteqal", "anar"}
+	//fmt.Println(miveha[0])
+	//
+	//grad := [5]int{70, 80, 90, 10, 50}
+	//fmt.Println(grad)
+	//grad[2] = 510501
+	//fmt.Println(grad)
+
+	//testakar := [3]int{1, 8, 2}
+	//for i := 0; i < len(testakar); i++ {
+	//	fmt.Println(i)
+	//}
+
+	//daraje := [5]int{10, 20, 30, 50, 60}
+	//for index, element := range daraje {
+	//	fmt.Println(index, "==>", element)
+	//}
+	//00000000000000000000000000000000000000
+	//array := [3][2]int{{1, 2}, {3, 4}, {5, 6}}
+	//fmt.Println(array[2][1])
+
+	//arr := [6]int{10, 20, 20, 30, 50, 40}
+	//slice := arr[1:5]
+	//fmt.Println(slice)
+
+	//slice := make([]int, 1, 10) //make کارش اینطوریه اولی تایپ میده دومی لنگ سومی کاپ
+	//fmt.Println(slice)
+	//fmt.Println(len(slice))
+	//fmt.Println(cap(slice))
+
+	//arr := [4]int{10, 20, 30, 40}
+	//slice := arr[1:3]
+	//fmt.Println(slice)
+	//fmt.Println(len(slice))
+	//fmt.Println(cap(slice))
+
+	//slice = append(slice, 0550, 5598, 9875) //با این میشه که اسلایس های دیگه هم توی هم ریخت به جای هر کدوم از عددها اسم یه اسلایس باشه
+	//fmt.Println(slice)
+	//fmt.Println(len(slice))
+	//fmt.Println(cap(slice))
+
+	//arr := [4]int{10, 20, 30, 40}
+	//i := 2
+	//fmt.Println(arr)
+	//slice := arr[:i]
+	//slicee := arr[i+1:]
+	//sliceakhar := append(slice, slicee...)
+	//fmt.Println(sliceakhar)
+	//
+	//00000000000000000000000000
+	//جلسه بعدی
+	//cods := map[string]string{"en": "english", "fr": "france"}
+	//fmt.Println(cods)
+	//fmt.Println(cods["en"])
+	//fmt.Println(len(cods))
+	//
+	//cods := map[string]int{"en": 1, "fr": 2, "sp": 3}
+	//value, fuond := cods["sp"]
+	//fmt.Println(value, fuond)
+	//value, fuond = cods["hg"]
+	//fmt.Println(value, fuond)
+	//cods["it"] = 4 //ااین کار میشه ادد کرذ همینطور اصلاح
+	//fmt.Println(cods)
+	//cods["en"] = 6542
+	//fmt.Println(cods)
+	//delete(cods, "en")
+	//fmt.Println(cods)
+	//0000000000000000000000000000000000000000000000
+	//sum, diff := addnumbers(10, 20)
+	//fmt.Print(sum, diff, "\n")
+	////00000
+	//mamad("mehdi")
+	//
+	////0000
+	//fmt.Println(sumjadide())
+	//fmt.Println(sumjadide(10, 4141, 40, 0, 00, 0, 00, 04, 4, 5241, 6))
+	//00000000000
+	x:=
+
 }
 
-func mamad() {
-
-	fmt.Println("Mehdi the Best")
-}
+//func addnumbers(a int, b int) (int, int) {
+//	sum := a + b
+//	diff := a - b
+//	return sum, diff
+//
+//}
+//func mamad(str string) {
+//	fmt.Println("salam", str)
+//}
+//func sumjadide(numbers ...int) int {
+//	sum := 0
+//	for _, x := range numbers {
+//		sum += x
+//	}
+//	return sum
+//}
+//func f() (int, int) {
+//	return 42, 41
+//}
